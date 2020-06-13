9b151b909c0bc4f72bf95861c4f45d075645eea5
kfrajer <krisfrajer@gmail.com>
2020-06-13 08:55:01 -0400

    Added migration and model

---
+++ app/MemObject.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MemObject extends Model
+{
+    //
+}
---
+++ database/migrations/2020_06_13_013838_create_memobjects_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemobjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memobjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',128);
+            $table->text('description');
+            $table->enum('type',array('text/plain','text/html','text/csv','text/css','text/xml','application/javascript','application/pdf','application/octet-stream','application/json','application/zip','application/x-www-form-urlencoded','image/gif','image/jpeg','image/png','image/tiff','image/svg+xml','multipart/mixed','multipart/form-data'));
+            $table->string('uri',256);
+            //$table->binary('data');
+            $table->integer('ttl');
+            $table->string('tags',64);
+            $table->string('acl',64);
+            $table->softDeletes('deleted_at', 0);
+            $table->boolean('useOnlyOnce');
+            $table->timestamps();
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memobjects');
+    }
+}
