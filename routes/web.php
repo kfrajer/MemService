ee4fca94a724ca61c7a7d253287591244b4c7e15
kfrajer <krisfrajer@gmail.com>
2020-06-13 08:54:00 -0400

    Defined routes, added controllers: pages

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,13 @@ Route::get('/','PagesController@index')->name('pages.index');
 Route::get('/about','PagesController@about')->name('pages.about');
 
 Route::get('/signin','SessionController@signin')->name('pages.signin');
+
+Route::resource('/memobjects','menuStorageController');
+//Route::get('/memobjects','menuStorageController@index')->name('memobjects.index');
+//Route::get('/memobjects/create','menuStorageController@create')->name('memobjects.create');
+//Route::post('/memobjects','menuStorageController@store')->name('memobjects.store'); // making a post request
+//Route::get('/memobjects/{id}','menuStorageController@show')->name('memobjects.show');
+//Route::get('/memobjects/{id}/edit','menuStorageController@edit')->name('memobjects.edit');
+//Route::put('/memobjects/{id}','menuStorageController@update')->name('memobjects.update'); // making a put request
+//Route::delete('/memobjects/{id}','menuStorageController@destroy')->name('memobjects.destroy'); // making a delete request
+
